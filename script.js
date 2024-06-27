cb28cf77ea60ccd0a8c6cf8b22992e653d7435a1
itisaarjan <arjansubedi@usf.edu>
2024-06-26 21:51:51 -0400

    Completed

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
-let clutter=""
-
-for (let i=0;i<168;i++){
-    clutter+=`<div class="bubble">${Math.floor(Math.random()*10)}</div>`
+let score=0;
+let hitrn=0;
+function increaseScore(){
+    score+=10;
+    document.querySelector("#score-val").textContent=score;
 }
-document.querySelector("#pbottom").innerHTML=clutter;
+function makeBubble(){
+    let clutter=""
+
+    for (let i=0;i<168;i++){
+    clutter+=`<div class="bubble">${Math.floor(Math.random()*10)}</div>`
+    }
+    document.querySelector("#pbottom").innerHTML=clutter;
+}
+let timer=60;
+function runTimer(){
+    let timerint=setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector(".timer-value").textContent=timer;
+        }else{
+            clearInterval(timerint);
+            document.querySelector("#pbottom").innerHTML="";
+        }
+        
+    },1000);
+}
+function getNewHit(){
+    hitrn=Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent=hitrn;
+}
+
+document.querySelector("#pbottom").addEventListener("click",function (details){
+    let clickednum=Number(details.target.textContent);
+    if(clickednum===hitrn){
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+})
+
+runTimer();
+makeBubble();
+getNewHit();
